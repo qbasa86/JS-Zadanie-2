79188f2332d521c0c4acf6c6274befdcd7663e2e
qbasa <qbasa@wp.pl>
2017-04-20 15:00:06 +0200

    dodanie komentarzy

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-var name = prompt("Wpisz swoje imię");
-alert("Siema, " + name);
-console.log("witaj, " + name);
+var name = prompt("Wpisz swoje imię");//zmianna name
+alert("Siema, " + name);//funkcja alert
+console.log("witaj, " + name);/*Blokowy
+komentarz*/
